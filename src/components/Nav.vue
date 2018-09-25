df5c09ca6cffeb18344ef395b3f1ca7fbea72db6
Alex Valera <alexv@tahzoo.com>
2018-09-24 22:09:31 -0400

    initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    hello
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang = "scss" scoped>
+nav {
+  height: 50px; 
+  background-color: #1abc9a;
+}
+</style>
